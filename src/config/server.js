457a51bdd17e2d86d315acb0a749de92af4a0120
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-20 12:08:08 -0300

    config server files

--- src/config/server.js
+++ src/config/server.js
@@ -3,10 +3,16 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
+const routes = require('./routes')
 
-server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(routes)
 
-server.listen(port, function(){
-    console.log(`Servidor rodando na porta ${port}`)
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
