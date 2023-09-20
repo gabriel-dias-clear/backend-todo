8615bc4db8cce36fa7e373343b195c4814642e73
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-20 12:07:46 -0300

    mongo connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports =  mongoose.connect("mongodb://localhost:27017/todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
