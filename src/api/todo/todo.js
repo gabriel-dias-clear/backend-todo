2f1c316722cb4e7ba092a44f8477c5583b431875
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-20 12:06:58 -0300

    todo schema

--- src/api/todo/todo.js
+++ src/api/todo/todo.js
@@ -1,10 +1,15 @@
-const restful = require('node-restful')
-const mongoose = restful.mongoose
 
-const todoSchema = new mongoose.todoSchema({
+const mongoose = require('mongoose');
+const {Schema} = mongoose
+
+const todoSchema = new Schema({ 
     description: { type: String, required: true },
     done: { type: Boolean, required: true, default: false },
     createdAt:{ type: Date, default: Date.now }
-})
+ });
 
-module.exports = restful.model('Todo', todoSchema)
+ const Task = mongoose.model('Task', todoSchema)
+
+
+
+module.exports = Task
